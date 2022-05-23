16668d3eef2ce12014d048dab1a892013957955e
8796454500 <ashwinerande2016@gmail.com>
2022-05-23 12:41:28 +0530

    DONE

---
+++ SpringBootCoreBase/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,29 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApplicationController {
+
+	@GetMapping
+	public String SayHello() {
+		return "welcome to csi pune";
+
+	}
+
+	@GetMapping("/service")
+	public String Sayservices() {
+		return "software developer";
+
+	}
+
+	@GetMapping("/adress")
+	public String Sayadress() {
+		return "kolharwadi";
+
+	}
+
+}
